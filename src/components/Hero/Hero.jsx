2f20cd6f4db25b3e9cbf10262729fa54c3750e32
Raj Chauhan <83418720+Raj1208@users.noreply.github.com>
2024-04-06 11:18:42 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
               type: "ease-in",
             }}
             >
-              Discover <br />
+              Charusat <br />
               Most Suitable
               <br /> Property
             </motion.h1>
