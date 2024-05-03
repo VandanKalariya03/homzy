10d53b4dfe7a8fc64cf12a8637746dcb5a111900
VandanKalariya03 <132541490+VandanKalariya03@users.noreply.github.com>
2024-05-04 01:20:11 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
               type: "ease-in",
             }}
             >
-              homzy <br />
+              homzy11 <br />
               Most Suitable
               <br /> Property
             </motion.h1>
