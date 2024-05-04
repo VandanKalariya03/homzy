b987a7b8a6746c3e5f1c9b196788080f86ba7490
VandanKalariya03 <132541490+VandanKalariya03@users.noreply.github.com>
2024-05-04 14:03:18 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
               type: "ease-in",
             }}
             >
-              depstar india <br />
+              Charusat <br />
               Most Suitable
               <br /> Property
             </motion.h1>
